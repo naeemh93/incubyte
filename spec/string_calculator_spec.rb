afb14e693c449f2af97beb0d88522fa44785ea35
Naeem Haider <naeem@Naeems-MacBook-Pro.local>
2024-07-11 20:55:56 +0100

    Handling single input number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context 'when input is a single number' do
+      it 'returns the number' do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
+
+
   end
 end
