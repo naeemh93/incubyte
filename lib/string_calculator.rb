f7696aeb496c72a65902452b8e58c26eafcc46c3
Naeem Haider <naeem@Naeems-MacBook-Pro.local>
2024-07-11 21:29:15 +0100

    Handled implementation for custom delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,21 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.gsub(/\n/, ',').split(',').map(&:to_i).sum
+    delimiter, numbers = fetch_delimiter(numbers)
+
+    numbers.split(delimiter).map(&:to_i).sum
+  end
+
+  private
+  def self.fetch_delimiter(numbers)
+    if numbers.start_with?('//')
+      delimiter_section, numbers = numbers.split("\n", 2)
+      delimiter = delimiter_section[2..]
+    else
+      delimiter = ','
+    end
+
+    numbers = numbers.gsub("\n", delimiter)
+    [delimiter, numbers]
   end
 end
